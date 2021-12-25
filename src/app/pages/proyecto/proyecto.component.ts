5c4c90110cf650c9c51cc5c8841f33d5ddb58cb0
Matias <matias.abarca2018@gmail.com>
2021-12-25 20:52:10 -0300

    algunos cambios en css

--- src/app/pages/proyecto/proyecto.component.ts
+++ src/app/pages/proyecto/proyecto.component.ts
@@ -9,6 +9,7 @@ export class ProyectoComponent implements OnInit {
 
   arr: number[] = [ 1,2,3,4]
 
+  badges: String[] = ['Angular','Bootstrap','JavaScript','NodeJS','C#','CCS'];
   constructor() { }
 
   ngOnInit(): void {
