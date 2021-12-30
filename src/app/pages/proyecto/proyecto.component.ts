047a6c7b40d355e86f2f6f9c5888c8032a86262e
Matias <matias.abarca2018@gmail.com>
2021-12-30 16:45:42 -0300

    avanzando en seccion proyectos

--- src/app/pages/proyecto/proyecto.component.ts
+++ src/app/pages/proyecto/proyecto.component.ts
@@ -11,17 +11,29 @@ export class ProyectoComponent implements OnInit {
   badges: string[] = ['Angular','Bootstrap','Firebase','JavaScript','NodeJS','C#','CCS','Ionic'];
  
   proyectosArr: Proyecto[] = [
-    { titulo:'Caja Chica App', badges: [ 
-      this.badges[0],this.badges[2]], 
-      imagen: '', repositorio:'', linkWeb:'' },
+    { 
+      titulo:'Caja Chica App', 
+      badges: [ this.badges[0],this.badges[2]], 
+      imagen: '../../assets/img/caja-chica/1.webp', 
+      repositorio:'https://github.com/matias-user/proyecto-innovacion', 
+      linkWeb:'https://caja-chica-bc982.web.app/' 
+    },
       
-    { titulo:'Tienda web', badges: [ 
-      this.badges[0],this.badges[2],this.badges[6] ], 
-      imagen: '', repositorio:'', linkWeb:'' },
+    { 
+      titulo:'Tienda web', 
+      badges: [ this.badges[0],this.badges[2],this.badges[6] ], 
+      imagen: '../../assets/img/carrito/1.webp', 
+      repositorio:'https://github.com/matias-user/proyecto-carrito', 
+      linkWeb:'https://carrito-a51a4.web.app/' 
+    },
     
-    { titulo:'Caja Chica App', badges: [ 
-      this.badges[1],this.badges[3]], 
-      imagen: '', repositorio:'', linkWeb:'' }
+    { 
+      titulo:'Viajes Chile', 
+      badges: [ this.badges[1],this.badges[3]], 
+      imagen: '../../assets/img/viajes-chile/1.webp', 
+      repositorio:'https://github.com/matias-user/viajes-chile', 
+      linkWeb:'https://matias-user.github.io/viajes-chile/' 
+    }
   
 ] 
 
