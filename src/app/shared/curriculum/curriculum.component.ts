4d93cbadf406ce576957d7920ab15bb56e87c8c6
Matias <matias.abarca2018@gmail.com>
2022-01-15 22:24:51 -0300

    avanzando en la seccion curriculum

--- src/app/shared/curriculum/curriculum.component.ts
+++ src/app/shared/curriculum/curriculum.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurriculumComponent implements OnInit {
 
+  tecnologias: any[] = [ 
+    { linkImagen:'../../assets/img/lenguajes/bootstrap.svg', nombre:'Bootstrap' },
+    { linkImagen:'../../assets/img/lenguajes/angular-icon.svg', nombre:'Angular' },
+    { linkImagen:'../../assets/img/lenguajes/c-sharp.svg', nombre:'C#' },
+    { linkImagen:'../../assets/img/lenguajes/express.svg', nombre:'Express' },
+    { linkImagen:'../../assets/img/lenguajes/javascript.svg', nombre:'Javascript' },
+    { linkImagen:'../../assets/img/lenguajes/mongodb.svg', nombre:'Mongodb' },
+    { linkImagen:'../../assets/img/lenguajes/nodejs-icon.svg', nombre:'Nodejs' },
+
+   ];
+  urlImagenPerfil: string = '../../assets/img/perfil.webp';
+  linkPortafolio: string = 'portafolio-4c8e2.web.app';
+  linkGitHub: string = 'https://github.com/matias-user';
   constructor() { }
 
   ngOnInit(): void {
